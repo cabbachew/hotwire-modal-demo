6d8a93dda2a609542d6e10423d1ac0e322d76948
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-27 14:30:36 -0800

    implement stimulus actions to close modal with escape key or background click

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -2,17 +2,34 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="turbo-modal"
 export default class extends Controller {
+  static targets = ["modal"]
+
   connect() {
   }
 
+  // When close button is clicked
   hideModal() {
-    this.element.parentElement.removeAttribute("src")
+    this.element.parentElement.removeAttribute("src") // Enables re-opening on subsequent clicks
     this.element.remove()
   }
 
+  // When form is submitted
   submitEnd(e) {
     if (e.detail.success) {
       this.hideModal()
     }
+  }
+
+  closeWithEscape(e) {
+    if (e.key === "Escape") {
+      this.hideModal()
+    }
+  }
+
+  closeBackground(e) {
+    if (e && this.modalTarget.contains(e.target)) {
+      return
+    }
+    this.hideModal()
   }
 }
